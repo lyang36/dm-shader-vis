84a80b6a19946d54a8d2d448731f26b1bc20b3b4
lyang.thu <lyang.thu@gmail.com>
2012-07-27 14:57:02 -0400

    for win

---
+++ gettime.h
@@ -0,0 +1,21 @@
+#ifndef __LY_GETTIME__
+#define __LY_GETTIME__
+#include <time.h>
+#include <windows.h>
+#include <iostream>
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+#else
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+#endif
+
+struct timezone
+{
+    int  tz_minuteswest; /* minutes W of Greenwich */
+    int  tz_dsttime;     /* type of dst correction */
+};
+
+
+int gettimeofday(struct timeval *tv, struct timezone *tz);
+
+#endif
